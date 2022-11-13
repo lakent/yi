b47a5e317d61e5f0740c7225420160da7bf39357
小尛star <m19357513004@163.com>
2022-11-13 20:03:13 +0800

    add full code translates simple code

--- yi_table_generator.py
+++ yi_table_generator.py
@@ -2,7 +2,6 @@
 
 etymon = []
 dic = []
-codeset = []
 
 # query components(be queried)
 def fname(arg):
@@ -11,51 +10,70 @@ def fname(arg):
         if z == arg:
             return etymon[x][1]
 
-def code(arg):
-        for index in range(len(arg)):
-            x = arg[0:index + 2]
-            if (index == 5):
-                shortcode = x 
+def translatesCode():
+    dicCount = len(dic)
+    for i in range(dicCount):
+        s = ''
+        for j in range(len(dic[i][1])):
+            ch = dic[i][1][j]
+            s=s+fname(ch)
+        dic[i][1] = s
+
+def readTable(path):
+    try:
+        readContent = []
+        for line in open(path, 'r', encoding='utf-8'):
+            l = line.split('\t')
+            # l -> components, keyboard key
+            l[1] = l[1][0:-1]
+            readContent.append([l[0],l[1]])
+        return readContent
+    except Exception:
+        print(Exception)
+
+def writeTable(path):
+    table_file = open(path, "w", encoding='utf-8')
+    for i in dic:
+        character=i[0]
+        code=i[1]
+        table_file.write(character+"\t"+ code +"\n")
+
+    table_file.close()
+
+def simpleWord():
+    flag = []
+
+    for i in range(len(dic)):
+        t = dic[i][1][0:2]
+        if t in flag: continue
+        count = 2
+        for j in range(len(dic)):
+            currentWord = dic[j][1]
+            if t == currentWord[0:2]:
+                dic[j][1] = dic[j][1][0:count]
+                count = count + 1
+            if count >= 5:
                 break
-            elif (x in codeset):
-                pass
-            else:
-                codeset.append(x)
-                shortcode = x 
-                break
-        return shortcode
 
-# read components mapping
-for line in open('yi_components_mapping.txt',"r",encoding='utf-8'):
-    l = line.split('\t')
-    # l -> components, keyboard key
-    l[1] = l[1][0:-1]
-    etymon.append([l[0],l[1]])
+if __name__ == '__main__':
+    # read components mapping
+    etymon = readTable('yi_components_mapping.txt')
 
-# read spelling
-for line in open('yi_spelling.txt',"r",encoding='utf-8'):
-    l = line.split('\t')
-    # l -> character，spelling
-    l[1] = l[1][0:-1]
-    dic.append([l[0],l[1]])
+    # read spelling
+    dic = readTable('yi_spelling.txt')
 
-# deal
-for i in range(len(dic)):
-    s = ''
-    for j in range(len(dic[i][1])):
-        ch = dic[i][1][j]
-        s=s+fname(ch);
-    dic[i][1] = s
+    # spelling translates code
+    translatesCode()
 
-# write code file
-fullcode_file = open("yi_fullcode_table.txt","w",encoding='utf-8')
-table_file = open("yi_table.txt","w",encoding='utf-8')
-for i in dic:
-    character=i[0]
-    fullcode=i[1]
-    shortcode = code(fullcode);
-    fullcode_file.write(character+"\t"+ fullcode +"\n")
-    table_file.write(character+"\t"+ shortcode +"\n")
+    # write codeTable
+    fullCodeTablePath = "yi_fullcode_table.txt" 
+    writeTable(fullCodeTablePath)
+    print("write fullcode to " + fullCodeTablePath)
 
-print("write fullcode to 'yi_fullcode_table.txt'")
-print("write table to 'yi_table.txt'")
+    # full code translates simple code
+    simpleWord()
+
+    # write SimpleCodeTable
+    simpleCodeTablePath = "yi_table.txt"
+    writeTable(simpleCodeTablePath)
+    print("write table to " + simpleCodeTablePath)
